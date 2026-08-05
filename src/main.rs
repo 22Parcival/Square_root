55ffd32bd13b9f26b6c5e5e472b54f2bebf37596
22Parcival <241494785+22Parcival@users.noreply.github.com>
2026-08-05 12:55:50 +0200

    bloc structure Cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    input: String,
+    #[arg(shorT, long)]
+    output: String,
 }
