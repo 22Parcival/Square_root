2eae1e31cf42244f5ec6a3f26a24b946261c56f5
22Parcival <241494785+22Parcival@users.noreply.github.com>
2026-08-06 15:51:58 +0200

    ajout const masco

--- web_lecteur/script.js
+++ web_lecteur/script.js
@@ -1,3 +1,6 @@
 const fileInput = document.querySelector('input[type="file"]');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+const mascot = new Image();
+mascot.src = 'rt1.png';
